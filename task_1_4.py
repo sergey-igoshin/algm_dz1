eca749f876726f05d9fd91d9d6b54cbc977b5b41
Сергей Игошин <sergey@csb-mirena.ru>
2022-01-31 20:24:14 +0300

    modified task_1_4

--- task_1_4.py
+++ task_1_4.py
@@ -22,30 +22,58 @@
 import bcrypt
 
 
+# O(n**2)
 def hash_password(a):
-    users_hash = {}
-    for key, val in a.items():
-        hash_salt = bcrypt.hashpw(val.encode('utf-8'), bcrypt.gensalt(8))
+    users_hash = {}     # O(1)
+    for key, val in a.items():  # O(n)
+        hash_salt = bcrypt.hashpw(val.encode('utf-8'), bcrypt.gensalt(8)) # O(1+n)
         users_hash[key] = {
-            'hash': hash_salt,
-            'action': False
-        }
-    return users_hash
+            'hash': hash_salt,  # O(1)
+            'action': 0     # O(1)
+        }   # O(1)
+    return users_hash   # O(1)
+# T(n) = 1+n*(1+n+1+1)+1 = n**2+3n+2
 
 
-def valid(a):
-    login = input('Логин: ')
-    password = input('Пароль: ')
-    hash = a[login]['hash']
-    if bcrypt.checkpw(password.encode('utf-8'), hash):
-        return 'ok'
-    else:
-        return 'no'
+# O(n)
+def valid(login, password, hash):
+    h = hash[login]['hash']     # O(1)
+    action = hash[login]['action']  # O(1)
+    if bcrypt.checkpw(password.encode('utf-8'), h):     # O(n)
+        if action == 1:     # O(n)
+            return 1    # O(1)
+        else:   # O(1)
+            return actions(login, password, hash)   # O(1)
+    else:   # O(1)
+        return 0    # O(1)
+# T(n) = 1+1+n+n+1 = 2n+3
 
+
+# O(n)
+def actions(login, password, hash):
+    action = input('Необходима активировация y/n: ')    # O(1)
+    if action == 'y':   # O(n)
+        hash[login]['action'] = 1   # O(1)
+        return valid(login, password, hash)     # O(1)
+    else:   # O(1)
+        return 2    # O(1)
+# T(n) = 1+n+1+1 = n+3
 
 users = {
-    'sergey@mail.ru': 'pass',
-    'oleg': 'pass2'
+    'sergey@mail.ru': 'pass',   # O(1)
+    'oleg@mail.ru': 'pass2'     # O(1)
 }
-hash = hash_password(users)
-print(valid(hash))
+hash = hash_password(users)     # O(1)
+
+while True:
+    login = input('Логин: ')    # O(1)
+    password = input('Пароль: ')    # O(1)
+    v = valid(login, password, hash)    # O(1)
+    if v == 1:  # O(n)
+        print('Доступ открыт')  # O(1)
+        break   # O(1)
+    elif v == 0:    # O(n)
+        print('Пара логин пароль не правильные, проверьте раскладку')   # O(1)
+    else:   # O(1)
+        print('Вы вышли из системы')    # O(1)
+        break   # O(1)
